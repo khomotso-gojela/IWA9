39b31029b5c27c871fa3109840fd01fb75fc7f10
khomotso gojela <motsoak49@gmail.com>
2023-10-02 10:44:50 +0200

    updated jsdoc

--- scripts.js
+++ scripts.js
@@ -1,46 +1,21 @@
-// const salary = 4000;
-// const lodging = 'apartment'
-// const size = 'large'
+// @ts-check
 
-
-// // Only change the syntax below (not the values or key names)
-
-// const expenses = {
-//     food: 51.7501,
-//     transport:  10.2,
-// }
-  
-// const tax = {
-//     734: '3%',
-//     234: '20%',
-//     913: '12%',
-//     415: '38%',
-//     502: '42%',
-// }
-
-// const rent = {
-//     none: 0,
-//     'small-room': 200,
-//     'large-room': 300,
-//     'small-apartment': 400,
-//     'large-apartment': 800,
-//     'small-house': 1200,
-//     'large-house': 2400,
-// }
-
-// // You can change below however you want
-
-// const taxAsDecimal = tax.913 / 100
-// const startingAfterTax = salary * 1 - taxAsDecimal
-// const type = lodging + size
-// const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
-// console.log(balance)
-/*****************************************************************************************/
-
-// solution
-
+/**
+ * Total salary
+ * @type {number}
+ */
 const salary = 4000;
+
+/**
+ * Type of lodge
+ * @tyoe {string}
+ */
 const lodging = 'apartment'
+
+/**
+ * Lodge size
+ * @type {string}
+ */
 const size = 'large'
 
 
@@ -72,7 +47,21 @@ const rent = {
 // You can change below however you want
 
 const taxAsDecimal = parseInt(tax['913']) / 100
+
 const startingAfterTax = salary - (salary * taxAsDecimal)
-const type = `${size}-${lodging}`
-const balance = startingAfterTax - expenses.transport - expenses.food - rent[type]
+
+//const type = `${size}-${lodging}`
+
+/**
+ * Make lodge type using lodge and size of the lodge.
+ * @param {string} sizeOfLodge
+ * @param {string} lodging 
+ * @returns {string}
+ */
+const lodgeType = (sizeOfLodge,lodging) => {
+    return `${sizeOfLodge}-${lodging}`
+}
+
+const balance = startingAfterTax - expenses.transport - expenses.food - rent[lodgeType(size,lodging)]
+
 console.log(balance.toFixed(2))
